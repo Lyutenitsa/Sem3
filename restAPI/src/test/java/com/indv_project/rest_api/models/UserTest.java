4194d75e89c0a7bc1e4106ad7cd528f4c66def03
Lyutenitsa <lutfi.ismail5@gmail.com>
2021-12-06 17:05:12 +0100

    new unit test and sonarqube addition

---
+++ restAPI/src/test/java/com/indv_project/rest_api/models/UserTest.java
@@ -0,0 +1,63 @@
+package com.indv_project.rest_api.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void get()
+    {
+    }
+
+    @Test
+    void setId()
+    {
+    }
+
+    @Test
+    void setUsername()
+    {
+    }
+
+    @Test
+    void setEmail()
+    {
+    }
+
+    @Test
+    void setPassword()
+    {
+    }
+
+    @Test
+    void setRoles()
+    {
+    }
+
+    @Test
+    void getId()
+    {
+    }
+
+    @Test
+    void getUsername()
+    {
+    }
+
+    @Test
+    void getEmail()
+    {
+    }
+
+    @Test
+    void getPassword()
+    {
+    }
+
+    @Test
+    void getRoles()
+    {
+    }
+}
