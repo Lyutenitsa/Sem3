5409e07da4507232f7076b1ca49c9a59ee24b3e9
Lyutenitsa <lutfi.ismail5@gmail.com>
2021-12-21 03:05:55 +0200

    docker files updated

--- restAPI/src/main/java/com/indv_project/rest_api/repositories/IRoleRepository.java
+++ restAPI/src/main/java/com/indv_project/rest_api/repositories/IRoleRepository.java
@@ -3,9 +3,11 @@ package com.indv_project.rest_api.repositories;
 import com.indv_project.rest_api.models.ERole;
 import com.indv_project.rest_api.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IRoleRepository extends JpaRepository<Role,Long> {
     Optional<Role> findByName(ERole name);
 }
